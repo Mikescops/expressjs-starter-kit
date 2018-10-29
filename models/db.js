ad394bb5a13177ca41d7cc7f5f067bed66b75bda
Corentin <mikescops@pixelswap.fr>
2018-10-29 11:15:38 +0100

    Add view and edit functions / template

--- models/db.js
+++ models/db.js
@@ -1,2 +1,3 @@
 var mongoose = require('mongoose');
+mongoose.set('useFindAndModify', false); // This prevent current mongoose depreciation warning
 mongoose.connect('mongodb://localhost/starterkit', {useNewUrlParser: true });
