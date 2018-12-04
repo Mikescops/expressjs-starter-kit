0a32f94159dbfc6ba39514585714d36d0fed2140
Corentin <mikescops@pixelswap.fr>
2018-12-04 17:26:08 +0100

    Add new gulp builder

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp'),
     spawn = require('child_process').spawn,
+    minify = require('gulp-minify'),
+    cleanCSS = require('gulp-clean-css'),
+    rename = require('gulp-rename'),
     node;
+
+const build = ['minify-css', 'minify-js', 'server'];
+
 
 /**
  * $ gulp server
- * description: launch the server. If there's a server already running, kill it.
+ * description: launch the server. If there's a server already running, kill it
  */
 gulp.task('server', function() {
   if (node) node.kill()
@@ -20,17 +26,42 @@ gulp.task('server', function() {
  * $ gulp
  * description: start the development environment
  */
-gulp.task('default', function() {
-  gulp.run('server')
+gulp.task('default', build, function() {
+  gulp.watch(['./app.js', './**/*.js', './**/*.html'], build); 
+});
 
-  gulp.watch(['./app.js', './**/*.js', './**/*.html'], function() {
-    gulp.run('server')
-  })
-  
-  // Need to watch for sass changes too? Just add another watch call!
-  // no more messing around with grunt-concurrent or the like. Gulp is
-  // async by default.
-})
+
+/**
+ * $ gulp minify-js
+ * description: compress all js files and output them in the dist folder
+ */
+gulp.task('minify-js', function() {
+  gulp.src('public/assets/js/*.js')
+    .pipe(minify({
+        noSource : true,
+        ext:{
+            src:'-debug.js',
+            min:'.min.js'
+        },
+        ignoreFiles: ['.min.js']
+    }))
+    .pipe(gulp.dest('public/assets/dist'))
+});
+
+
+/**
+ * $ gulp minify-css
+ * description: compress all css files and output them in the dist folder
+ */
+gulp.task('minify-css', () => {
+  return gulp.src('public/assets/css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('public/assets/dist'));
+});
+
 
 // clean up if an error goes unhandled.
 process.on('exit', function() {
