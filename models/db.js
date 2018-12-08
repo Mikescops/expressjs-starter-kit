c5d548c78c5ab0abe39eda9622bd2fc154c14944
Corentin <mikescops@pixelswap.fr>
2018-12-08 18:45:53 +0100

    Fix warning in console about mongoose

--- models/db.js
+++ models/db.js
@@ -1,3 +1,5 @@
 var mongoose = require('mongoose');
+
+mongoose.set('useCreateIndex', true); // This pervent collection.ensureIndex depreciation warning
 mongoose.set('useFindAndModify', false); // This prevent current mongoose depreciation warning
 mongoose.connect('mongodb://localhost/starterkit', {useNewUrlParser: true });
