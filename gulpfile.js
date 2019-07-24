5a2de756e117ae9c053aa7a0ca80d9221ce4cfdb
Corentin Mors <corentin.mors@dashlane.com>
2019-07-24 14:57:12 +0200

    Fix gulp build of assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,74 @@
 var gulp = require('gulp'),
-    spawn = require('child_process').spawn,
-    minify = require('gulp-minify'),
-    cleanCSS = require('gulp-clean-css'),
-    rename = require('gulp-rename'),
-    node;
-
-const build = ['minify-css', 'minify-js', 'server'],
-      run = ['server'],
-      minifyCSS = ['minify-css'],
-      minifyJS = ['minify-js'];
-
+	spawn = require('child_process').spawn,
+	minify = require('gulp-minify'),
+	cleanCSS = require('gulp-clean-css'),
+	rename = require('gulp-rename'),
+	node;
 
 /**
  * $ gulp server
  * description: launch the server. If there's a server already running, kill it
  */
-gulp.task('server', function() {
-  if (node) node.kill()
-  node = spawn('node', ['app.js'], {stdio: 'inherit'})
-  node.on('close', function (code) {
-    if (code === 8) {
-      gulp.log('Error detected, waiting for changes...');
-    }
-  });
+gulp.task('server', async () => {
+	if (node) node.kill()
+	node = spawn('node', ['app.js'], { stdio: 'inherit' })
+	node.on('close', (code) => {
+		if (code === 8) {
+			gulp.log('Error detected, waiting for changes...');
+		}
+	});
 })
-
-
-/**
- * $ gulp
- * description: start the development environment
- */
-gulp.task('default', build, function() {
-  gulp.watch(['./public/assets/css/*.css'], minifyCSS);
-  gulp.watch(['./public/assets/js/*.js'], minifyJS);
-  gulp.watch(['./app.js', './**/*.js', './**/*.html'], build); 
-});
-
 
 /**
  * $ gulp minify-js
  * description: compress all js files and output them in the dist folder
  */
-gulp.task('minify-js', function() {
-  gulp.src('public/assets/js/*.js')
-    .pipe(minify({
-        noSource : true,
-        ext:{
-            src:'-debug.js',
-            min:'.min.js'
-        },
-        ignoreFiles: ['.min.js']
-    }))
-    .pipe(gulp.dest('public/assets/dist'))
+gulp.task('minify-js', async () => {
+	gulp.src('public/assets/js/*.js')
+		.pipe(minify({
+			noSource: true,
+			ext: {
+				src: '-debug.js',
+				min: '.min.js'
+			},
+			ignoreFiles: ['.min.js']
+		}))
+		.pipe(gulp.dest('public/assets/dist'))
 });
-
 
 /**
  * $ gulp minify-css
  * description: compress all css files and output them in the dist folder
  */
-gulp.task('minify-css', () => {
-  return gulp.src('public/assets/css/*.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('public/assets/dist'));
+gulp.task('minify-css', async () => {
+	return gulp.src('public/assets/css/*.css')
+		.pipe(cleanCSS({ compatibility: 'ie8' }))
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest('public/assets/dist'));
 });
 
+/**
+ * $ gulp build
+ * description: prepare all assets
+ */
+gulp.task('build', gulp.parallel([
+	'minify-css',
+	'minify-js',
+]));
+
+/**
+ * $ gulp
+ * description: start the development environment
+ */
+gulp.task('default', gulp.series('build', 'server', watch = () => {
+	gulp.watch(['./public/assets/css/*.css'], gulp.series('minify-css'));
+	gulp.watch(['./public/assets/js/*.js'], gulp.series('minify-js'));
+	gulp.watch(['./app.js', './**/*.js', './**/*.html'], gulp.series('server'));
+}));
 
 // clean up if an error goes unhandled.
-process.on('exit', function() {
-    if (node) node.kill()
-})
+process.on('exit', () => {
+	if (node) node.kill()
+});
