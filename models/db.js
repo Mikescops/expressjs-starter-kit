3eff39bbc426f8e51665c67cc58b70c786cf85ec
Corentin Mors <corentin.mors@dashlane.com>
2020-10-02 12:34:41 +0200

    Update deps and fix deprecates

--- models/db.js
+++ models/db.js
@@ -2,4 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.set('useCreateIndex', true); // This pervent collection.ensureIndex depreciation warning
 mongoose.set('useFindAndModify', false); // This prevent current mongoose depreciation warning
-mongoose.connect('mongodb://localhost/starterkit', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost/starterkit', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
